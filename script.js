68df823908c3d90af49a89a5475fffe15cbd8972
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2021-07-24 12:56:24 -0700

    updated files

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
-import "firebase/analytics";
-
-// Add the Firebase products that you want to use
-import "firebase/firestore";
-
 var video = document.querySelector("#videoElement");
 
+//function to access webcam feed
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
